c9985ff8dad09acd4984dd1d88b43a24e3327cf7
pedro21monteiro <95852464+pedro21monteiro@users.noreply.github.com>
2023-04-19 11:01:37 +0100

    kjj

--- ContextBuider/Program.cs
+++ ContextBuider/Program.cs
@@ -17,34 +17,38 @@ namespace ContextBuider
         
         static void Main(string[] args)
         {
-            string rabbitHost = System.Environment.GetEnvironmentVariable("RABBITHOST") ?? "192.168.28.86";
-            using var _contex = new ContextAwareDb();
-            var _logic = new Logic();
-
-            //---
-
-            var factory = new ConnectionFactory { HostName = rabbitHost };
-
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            channel.ExchangeDeclare(exchange: "ContinentalExchange", type: ExchangeType.Topic, durable: true);
-            // declare a server-named queue
-            var queueName = channel.QueueDeclare("", exclusive: true).QueueName;
-            channel.QueueBind(exchange: "ContinentalExchange", queue: queueName, routingKey: "#.#");
-
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
+            while (true)
             {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                var routingKey = ea.RoutingKey;
-                Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
-                _logic.readMessage(routingKey, message, _contex);
-          
-            };
-            channel.BasicConsume(queue: queueName,
-                                 autoAck: true,
-                                 consumer: consumer);
+                string rabbitHost = System.Environment.GetEnvironmentVariable("RABBITHOST") ?? "192.168.28.86";
+                using var _contex = new ContextAwareDb();
+                var _logic = new Logic();
+
+                //---
+
+                var factory = new ConnectionFactory { HostName = rabbitHost };
+
+                using var connection = factory.CreateConnection();
+                using var channel = connection.CreateModel();
+                channel.ExchangeDeclare(exchange: "ContinentalExchange", type: ExchangeType.Topic, durable: true);
+                // declare a server-named queue
+                var queueName = channel.QueueDeclare("", exclusive: true).QueueName;
+                channel.QueueBind(exchange: "ContinentalExchange", queue: queueName, routingKey: "#.#");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+                    _logic.readMessage(routingKey, message, _contex);
+
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+            }
+           
 
             //Console.WriteLine(" Press [enter] to exit.");
             //Console.ReadLine();

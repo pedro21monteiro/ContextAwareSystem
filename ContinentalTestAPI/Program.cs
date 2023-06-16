a5a042045945087f436b1fe55e6aced718a28f76
Rodrigo Pereira <28rodrigopereira@gmail.com>
2023-06-16 11:08:59 +0100

    remove dev env

--- ContinentalTestAPI/Program.cs
+++ ContinentalTestAPI/Program.cs
@@ -20,12 +20,10 @@ builder.Services.AddDbContext<ContinentalDb>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
